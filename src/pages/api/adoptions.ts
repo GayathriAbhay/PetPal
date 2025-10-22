d569548a9d30c4d8dfd7ac213ba8e906ba4b7ac9
gayathriabhay <gayathriabhay2005@gmail.com>
2025-10-22 18:15:14 +0530

    prisma client path

--- src/pages/api/adoptions.ts
+++ src/pages/api/adoptions.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma';  // adjust path to your prisma client
+import prisma from '@prisma\client';  // adjust path to your prisma client
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
